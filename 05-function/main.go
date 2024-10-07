e777ad98764005c66cbca9aedfe127672f34b6a1
sproshchaev <sproshchaev@gmail.com>
2024-10-07 12:02:47 +0500

    upd

--- 05-function/main.go
+++ 05-function/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
+	SqRoots()
+}
+
+func SqRoots() {
+	var a, b, c float64
+	fmt.Scanln(&a, &b, &c)
+	// Вычисление дискриминанта
+	d := b*b - 4*a*c
+
+	// Проверка дискриминанта
+	if d < 0 {
+		fmt.Println("0 0") // Нет корней
+		return
+	}
+
+	root1 := (-b + math.Sqrt(d)) / (2 * a)
+	root2 := (-b - math.Sqrt(d)) / (2 * a)
+
+	// Вывод корней
+	if d == 0 {
+		// Один корень
+		fmt.Printf("%.6f\n", root1)
+	} else {
+		// Два корня, выводим в порядке возрастания
+		if root1 > root2 {
+			root1, root2 = root2, root1
+		}
+		fmt.Printf("%.6f %.6f\n", root1, root2)
+	}
 }
