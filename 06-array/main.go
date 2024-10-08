7d701f7b0854454d1951ca254aade2de78c6ab04
sproshchaev <sproshchaev@gmail.com>
2024-10-08 10:43:37 +0500

    upd

--- 06-array/main.go
+++ 06-array/main.go
@@ -7,14 +7,17 @@ import (
 // Напишите функцию FiveSteps(array [5]int) [5]int, которая принимает массив из пяти целочисленных элементов.
 // Программа должна возвращать значения элементов массива в обратном порядке.
 func main() {
-	fmt.Println("Hello!")
 	var myArr [5]int
 	myArr = [5]int{0, 1, 2, 3, 4}
-	fmt.Println(FiveSteps(myArr))
+	result := FiveSteps(myArr)
+	fmt.Println(result)
+	for i := 0; i < len(result); i++ {
+		fmt.Printf("%d ", result[i])
+	}
 }
 
 func FiveSteps(array [5]int) [5]int {
-	var arr [len(array)]int
+	var arr [5]int
 	for i := len(array) - 1; i >= 0; i-- {
 		arr[len(array)-1-i] = array[i]
 	}
