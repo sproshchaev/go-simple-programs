d9ffc393f37ffbc1a44dbed0a91b18c741ea2608
sproshchaev <sproshchaev@gmail.com>
2024-10-09 18:15:42 +0500

    add

--- 07-slice/main4.go
+++ 07-slice/main4.go
@@ -4,18 +4,25 @@ import("fmt")
 
 func main() {
 
-	nums1 := make([]int, 2)
-	nums1[0] = 1
-	nums1[1] = 2
+	nums1 := []int{1, 2, 3}
 	fmt.Printf("Слайс #1 %d\n", nums1)
 	
-	nums2 := make([]int, 2)
-	nums2[0] = 1
-	nums2[1] = 2
+	nums2 := []int{4, 5, 6, 7}
 	fmt.Printf("Слайс #2 %d\n", nums2)
+
+	fmt.Printf("Join %d\n", Join(nums1, nums2))
 
 }
 
 // Даны 2 слайса целых чисел nums1 и nums2. Создайте функцию Join(nums1, nums2 []int) []int, 
 // которя создаст новый слайс емкостью, вмещающей в себя ровно два слайса (ёмкость должна быть равна его длине). 
 // Скопируйте в него сначала значения nums1 затем nums2 и верните его.
+func Join(nums1, nums2 []int) []int {
+    // Создаем новый слайс с длиной и емкостью равными сумме длин nums1 и nums2
+	newNums := make([]int, 0, len(nums1)+len(nums2))
+ 
+                                	// вариативный оператор ... - Троеточие после nums2 используется для распаковки элементов слайса nums2, чтобы они были добавлены как отдельные элементы, а не как один слайс
+	newNums = append(newNums, nums1...) // добавляем все элементы из nums1
+	newNums = append(newNums, nums2...) // добавляем все элементы из nums2
+	return newNums
+}
---
+++ 07-slice/main5.go
@@ -0,0 +1,15 @@
+package main
+
+import("fmt")
+
+func main() {
+
+	
+
+}
+
+// Дан слайс nums, состоящий из 2n элементов в формате [x0,x1,...,xn,y0,y1,...,yn]. 
+// Создайте функцию Mix(nums []int) []int, которая вернёт слайс, содержащий значения в следующем порядке: [x0,y0,x1,y1,...,xn,yn].
+func Mix(nums []int) []int {
+	return nil
+}
