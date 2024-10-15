c23cd12b4095e519e8f3893df962f99cf6749217
sproshchaev <sproshchaev@gmail.com>
2024-10-15 14:59:36 +0500

    add TimeDifference

---
+++ 13-time/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Date(2023, 10, 23, 2, 41, 49, 0, time.UTC)
+	end := time.Date(2023, 10, 23, 4, 41, 49, 0, time.UTC)
+
+	diff := TimeDifference(start, end)
+	fmt.Println(diff)
+}
+
+// Напишите функцию TimeDifference(start, end time.Time) time.Duration, которая вычисляет разницу во времени между двумя заданными моментами времени. 
+// Функция должна принимать на вход два значения time.Time и возвращать продолжительность между ними.
+// Примечания: Пример работы функции
+// start := time.Date(2023, 10, 23, 2, 41, 49, 0, time.UTC)
+// end := time.Date(2023, 10, 23, 4, 41, 49, 0, time.UTC)
+// diff := TimeDifference(start, end)
+// fmt.Println(diff) // Output: 2h0m0s
+func TimeDifference(start, end time.Time) time.Duration {
+	return end.Sub(start)
+}
